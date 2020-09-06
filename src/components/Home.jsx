88b8fb1bedf1223ebf086651d550a7e1beb6d15c
sina <sinasky1@gmail.com>
2020-09-06 23:22:35 +0430

    search feature by title of project added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,36 @@ import React, { Component, useEffect, setState } from "react";
 import { db } from "./firbase";
 class Home extends Component {
   state = {
+    searchInput: "",
+    allPosts: [],
     posts: [],
   };
 
+  onChangeHander = (e) => {
+    const searchInput = e.currentTarget.value;
+    this.setState({ posts: this.state.allPosts });
+    this.setState({ searchInput });
+  };
+  findTitle = (e) => {
+    const posts = [...this.state.allPosts];
+
+    const filltered = posts.filter((p) => p.title == this.state.searchInput);
+
+    this.setState({ posts: filltered });
+  };
   componentDidMount() {
     db.collection("Posts")
+      .get()
+      .then((data) => {
+        const allPosts = data.docs.map((p) => {
+          return p.data();
+        });
+        this.setState({ allPosts });
+        this.setState({ posts: allPosts });
+      });
+  }
+
+  /*db.collection("Posts")
       .get()
       .then((data) => {
         const posts = data.docs.map((p) => {
@@ -21,8 +46,7 @@ class Home extends Component {
           );
         });
         this.setState({ posts });
-      });
-  }
+      });*/
 
   render() {
     return (
@@ -33,14 +57,28 @@ class Home extends Component {
               className="form-control"
               placeholder="title ..."
               type="text"
+              onChange={this.onChangeHander}
             />
-            <button className="btn btn-success mb-1 rounded-left-0">
+            <button
+              className="btn btn-success mb-1 rounded-left-0 "
+              onClick={this.findTitle}
+            >
               search
             </button>
           </div>
         </div>
 
-        {this.state.posts}
+        {this.state.posts.map((p) => {
+          return (
+            <div className="card w-90 mt-5">
+              <p className="card-header">{p.Field}</p>
+              <div className="card-body">
+                <h1 className="card-title ">{p.title}</h1>
+                <p className="card-text">{p.Description}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
